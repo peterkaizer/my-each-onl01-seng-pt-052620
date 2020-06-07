54c969c21542cc3e52e3a4be784ce5c42bbb7acf
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-07 05:55:27 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  if block_given?
+    x = 0
+    while x < collection.length
+    yield(collection[x])
+    x = x + 1
+    end
+    collection
+  else
+    "Hey! No more Block bro!"
+  end
 end
